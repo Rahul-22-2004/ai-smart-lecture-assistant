2de16c6d9c5a6ac346c24a7bddfc9762375d8ebb
Rahul-22-2004 <rahuldgowda2004@gmail.com>
2026-02-23 23:58:52 +0530

    Updated uplaod endpoint in main file

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, UploadFile, File
 import os
 import shutil
 from app.speech import transcribe_audio
-from app.llm import generate_notes
+# from app.llm import generate_notes
+from app.llm import generate_notes, generate_quiz, generate_flashcards
 
 app = FastAPI()
 
@@ -23,13 +24,17 @@ async def upload_audio(file: UploadFile = File(...)):
         # 🎤 Transcription
         transcript = transcribe_audio(file_path)
 
-        # 🧠 AI Notes
+        # 🧠 AI generation
         notes = generate_notes(transcript)
+        quiz = generate_quiz(transcript)
+        flashcards = generate_flashcards(transcript)
 
         return {
             "filename": file.filename,
             "transcript": transcript,
-            "notes": notes
+            "notes": notes,
+            "quiz": quiz,
+            "flashcards": flashcards
         }
 
     except Exception as e:
