95482db13e4c0d41a587094411e4869f04bd9260
Rahul-22-2004 <rahuldgowda2004@gmail.com>
2026-02-23 23:57:45 +0530

    Updated llm file

--- app/llm.py
+++ app/llm.py
@@ -3,16 +3,13 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
-print("API KEY:", os.getenv("LLM_API_KEY"))
-
-client = OpenAI(api_key=os.getenv("LLM_API_KEY"))
-
+client = OpenAI(
+    api_key=os.getenv("LLM_API_KEY"),
+    base_url="https://api.sarvam.ai/v1" # Sarvam API endpoint
+)
 
 
 def generate_notes(transcript: str):
-    """
-    Generate summary and structured notes from transcript
-    """
 
     prompt = f"""
     You are an academic lecture assistant.
@@ -27,9 +24,51 @@ def generate_notes(transcript: str):
     """
 
     response = client.chat.completions.create(
-        model="gpt-4o-mini",   # can change later
+        model="sarvam-m",
         messages=[{"role": "user", "content": prompt}],
         temperature=0.3
+    )
+
+    return response.choices[0].message.content
+
+def generate_quiz(transcript: str):
+    prompt = f"""
+    Generate 5 MCQ questions from this lecture transcript.
+
+    Each question must include:
+    - Question
+    - 4 options (A,B,C,D)
+    - Correct answer
+    - Short explanation
+
+    Transcript:
+    {transcript}
+    """
+
+    response = client.chat.completions.create(
+        model="sarvam-m",
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.4
+    )
+
+    return response.choices[0].message.content
+
+def generate_flashcards(transcript: str):
+    prompt = f"""
+    Generate 5 study flashcards from this lecture.
+
+    Format:
+    Q:
+    A:
+
+    Transcript:
+    {transcript}
+    """
+
+    response = client.chat.completions.create(
+        model="sarvam-m",
+        messages=[{"role": "user", "content": prompt}],
+        temperature=0.4
     )
 
     return response.choices[0].message.content
