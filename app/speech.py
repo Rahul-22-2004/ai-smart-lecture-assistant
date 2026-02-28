822b8a623f58d0b6b1fa468db158afdcbb5b4198
Rahul-22-2004 <rahuldgowda2004@gmail.com>
2026-03-01 00:15:45 +0530

    Fixed Sarvam MIME type issue

--- app/speech.py
+++ app/speech.py
@@ -1,18 +1,26 @@
 import requests
 import os
+import mimetypes
 
 SARVAM_API_KEY = os.getenv("LLM_API_KEY")
 
 def transcribe_audio(file_path):
-    url = "https://api.sarvam.ai/speech-to-text"
+
+    url = "https://api.sarvam.ai/v1/speech-to-text"
 
     headers = {
         "Authorization": f"Bearer {SARVAM_API_KEY}"
     }
 
+    # Detect MIME type properly
+    mime_type, _ = mimetypes.guess_type(file_path)
+
+    if mime_type is None:
+        mime_type = "audio/mpeg"  # fallback
+
     with open(file_path, "rb") as audio_file:
         files = {
-            "file": audio_file
+            "file": (os.path.basename(file_path), audio_file, mime_type)
         }
 
         response = requests.post(url, headers=headers, files=files)
